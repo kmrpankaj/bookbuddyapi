ce8f61a70e4a3f3d2fdbe1d169e4074f3b34163e
Pankaj Kumar <pankaj1018+1@gmail.com>
2024-03-11 00:52:34 +0530

    call the listen method in the connect callback.

--- server.js
+++ server.js
@@ -1,31 +1,35 @@
-require('dotenv').config()
-var cors = require('cors')
+require('dotenv').config();
+var cors = require('cors');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+const port = process.env.PORT || 3001; // Added default port if process.env.PORT is not set
 
-mongoose.connect(process.env.DATABASE_URL)
-const db = mongoose.connection
-const port = process.env.PORT
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('connected to database')) // always use mongod --dbpath /srv/mongodb/
-app.use(cors())
-app.use(express.json())
+db.on('error', (error) => console.error(error));
+db.once('open', () => {
+    console.log('Connected to database');
+
+    // Start listening only after database connection is established
+    app.listen(port, () => console.log(`Server started at http://localhost:${port}`));
+});
+
+app.use(cors());
+app.use(express.json());
 
 // Available Routes
-const studentsRouter = require('./routes/students')
-app.use('/students', studentsRouter)
-'localhost:3001/students'
-const seatsRouter = require('./routes/seats')
-app.use('/seats', seatsRouter)
-'localhost:3001/seats'
-const bookingRouter = require('./routes/bookings')
-app.use('/bookings', bookingRouter)
-'localhost:3001/bookings'
-const emailRoutes = require('./routes/email')
-app.use('/email', emailRoutes)
+const studentsRouter = require('./routes/students');
+app.use('/students', studentsRouter);
+
+const seatsRouter = require('./routes/seats');
+app.use('/seats', seatsRouter);
+
+const bookingRouter = require('./routes/bookings');
+app.use('/bookings', bookingRouter);
+
+const emailRoutes = require('./routes/email');
+app.use('/email', emailRoutes);
+
 app.use('/uploads', express.static('uploads'));
-
-
-app.listen(port, () => console.log(`server Started at http://localhost:${port}`));
